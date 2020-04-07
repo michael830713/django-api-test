8ca97d900f2343bdf8ea9663dc5390df15e9748e
mike <michael830713@gmail.com>
2020-04-07 16:27:37 +0800

    first

---
+++ profiles_api/serializer.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers, viewsets
+
+from profiles_api import models
+
+
+class HelloSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+        def create(self, validated_data):
+            user = models.UserProfile.object.create_user(
+                email=validated_data['email'],
+                name=validated_data['name'],
+                password=validated_data['password'],
+            )
+            return user
+
+        def update(self, instance, validated_data):
+            """Handle updating user account"""
+            if 'password' in validated_data:
+                password = validated_data.pop('password')
+                instance.set_password(password)
+
+            return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
---
+++ profiles_api/views.py
@@ -0,0 +1,100 @@
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+
+from profiles_api import serializer, models, permissions
+
+
+class HelloApiView(APIView):
+    serializer_class = serializer.HelloSerializer
+
+    def get(self, request, format=None):
+        an_apiview = ['Use HTTP methods as function (get, post, patch,put, delete',
+                      'Is fjiwoqpfeqwjeoqwpjf', 'wfojpiqpwoiwqpw', 'wfiohpwifwhof', ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST, )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializer.HelloSerializer
+
+    def list(self, request):
+        a_viewset = ['this is a view set',
+                     'this is the first viewset',
+                     'this is the first time that I try to make a router',
+                     ]
+        return Response({'message': 'hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+
+        return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializer.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializer.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
